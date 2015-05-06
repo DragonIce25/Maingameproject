410a46989639b560652c22e53a1ba3cefc3ddb31
DragonIce25 <autobot1984@hotmail.co.uk>
2015-05-06 12:20:25 +0100

    Week1ReUpload
    Had a problem needed to reupload it again

---
+++ MoveWeek1/Assets/move.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour 
+{
+	public float moveSpeed = 10f;
+
+	void Update () 
+	{
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+}
